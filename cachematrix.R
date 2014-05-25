b1eea65a063fa96fec46679ff9a99a9f5b9eaf68
Rakudajin <rakudajin@gmail.com>
2014-05-26 03:27:15 +0400

    HW2 d2

---
+++ cachematrix.R
@@ -0,0 +1,38 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()){
+    
+    m <- NULL
+    
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    seti <- function(inverse) m <<- inverse
+    geti <- function() m
+    
+    list(set = set, get = get,
+         seti = seti,
+         geti = geti)
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+    
+    m <- x$geti()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    
+    data <- x$get()
+    
+    m <- solve(a, ...)
+    x$seti(m)
+}
